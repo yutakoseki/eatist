16c70c1578f974fd53c901e7bb0a25a41bceadbc
koseki <koseki@thinkingreed.co.jp>
2023-08-25 21:27:54 +0900

    一旦プッシュ

---
+++ client/src/app/context/auth.tsx
@@ -0,0 +1,41 @@
+"use client"
+// 全体で使いまわすための関数を作成
+import React, { ReactNode, useContext } from "react";
+
+interface AuthContextType {
+    login: (token: string) => void;
+    logout: () => void;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+const AuthContext = React.createContext<AuthContextType>({
+    login: () => {},
+    logout: () => {},
+});
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+// ローカルストレージにtokenをセット
+export const AuthProvider = ({ children }:AuthProviderProps) => {
+    const login = async (token: string) => {
+        localStorage.setItem("auth_token", token);
+    };
+
+    const logout = () => {
+        localStorage.removeItem("auth_token");
+    };
+
+    const value = {
+        login,
+        logout,
+    };
+
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    )
+};
--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navber from "./components/Navber";
+import { AuthProvider } from "./context/auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +14,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ja">
-            <body className={inter.className}>
-                <Navber />
-                {children}
-            </body>
+            <AuthProvider>
+                <body className={inter.className}>
+                    <Navber />
+                    {children}
+                </body>
+            </AuthProvider>
         </html>
     );
 }
